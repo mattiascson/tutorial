85f008f60fb5a1dda7d54bf366463bbd08c3529c
mattiascson <mattiascson@gmail.com>
2023-01-15 14:15:28 +0100

    Python and Git setup in RStudio

--- PythonRSetup.R
+++ PythonRSetup.R
@@ -1,6 +1,9 @@
 library(reticulate)
+library(usethis)
+usethis::use_git_config(user.name = "mattiascson", user.email = "mattiascson@gmail.com")
+gitcreds::gitcreds_set()
+#Use either use_python()
 use_python("/home/mattias/anaconda3/envs/tutorial/bin/python")
+#or use_condaenv()
 use_condaenv("tutorial")
 conda_install("tutorial","cupy") #GPU Array calculation as NumPy
-conda_install("tutorial","pandas")
-conda_install("tutorial","plotnine")
