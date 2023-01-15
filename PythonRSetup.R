c991f991114124c6d7c8b1d21008c64b76a74168
mattiascson <mattiascson@gmail.com>
2023-01-15 20:10:17 +0100

    Minor change Python Git setup

--- PythonRSetup.R
+++ PythonRSetup.R
@@ -3,7 +3,7 @@ library(usethis)
 usethis::use_git_config(user.name = "mattiascson", user.email = "mattiascson@gmail.com")
 gitcreds::gitcreds_set()
 #Use either use_python()
-use_python("/home/mattias/anaconda3/envs/tutorial/bin/python")
+#use_python("/home/mattias/anaconda3/envs/tutorial/bin/python")
 #or use_condaenv()
 use_condaenv("tutorial")
-conda_install("tutorial","cupy") #GPU Array calculation as NumPy
+#conda_install("tutorial","cupy") #GPU Array calculation as NumPy
